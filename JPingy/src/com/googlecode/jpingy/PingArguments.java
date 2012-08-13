9382076514c882d3e4a241113d7c34c0083d4632
contact@thomasgoossens.be <contact@thomasgoossens.be@3f92f96d-80fb-0637-3984-7b7715b30677>
2012-08-13 19:58:49 +0000

    git-svn-id: http://jpingy.googlecode.com/svn/trunk@4 3f92f96d-80fb-0637-3984-7b7715b30677

--- JPingy/src/com/googlecode/jpingy/PingArguments.java
+++ JPingy/src/com/googlecode/jpingy/PingArguments.java
@@ -1,4 +1,5 @@
 package com.googlecode.jpingy;
+
 public class PingArguments {
 	String url;
 	int repeat;
@@ -11,6 +12,43 @@ public class PingArguments {
 		this.repeat = repeat;
 		this.timeout = timeout;
 		this.payload_bytes = payload_bytes;
+	}
+
+	public PingArguments() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static class Builder {
+
+		private PingArguments arguments;
+
+		public Builder() {
+			this.arguments = new PingArguments();
+		}
+
+		public Builder url(String url) {
+			arguments.url = url;
+			return this;
+		}
+
+		public Builder repeat(int repeat) {
+			arguments.repeat = repeat;
+			return this;
+		}
+
+		public Builder timeout(long timeout) {
+			arguments.timeout = timeout;
+			return this;
+		}
+
+		public Builder bytes(int bytes) {
+			arguments.payload_bytes = bytes;
+			return this;
+		}
+
+		public PingArguments build() {
+			return arguments;
+		}
 	}
 
 	public String getCommand() {
--- JPingy/src/com/googlecode/jpingy/SampleCode.java
+++ JPingy/src/com/googlecode/jpingy/SampleCode.java
@@ -10,11 +10,16 @@ public class SampleCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		PingArguments arguments = new PingArguments("google.com", 1, 5000, 32);
-
+		PingArguments arguments = new PingArguments.Builder().url("google.com")
+				.timeout(5000).repeat(2).bytes(32).build();
+		
 		PingResult results = Ping.ping(arguments, Backend.UNIX);
 
-		System.out.println(results);
+		System.out.println("TTL: " + results.ttl);
+
+		System.out.println("RTT Minimum: " + results.rtt_min);
+
+		System.out.println("Received : " + results.received);
 	}
 
 }
